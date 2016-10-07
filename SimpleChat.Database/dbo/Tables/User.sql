befe221d7a0618d6955d07cd0cd01df83d899967
Mate <mate@anticevic.net>
2016-10-07 21:01:02 +0200

    Initial commit.

---
+++ SimpleChat.Database/dbo/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [Nickname]  NVARCHAR (50)  NOT NULL,
+    [Email]     NVARCHAR (200) NOT NULL,
+    [FirstName] NVARCHAR (100) NOT NULL,
+    [LastName]  NVARCHAR (100) NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ SimpleChat.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleChat.Web.MvcApplication" Language="C#" %>
